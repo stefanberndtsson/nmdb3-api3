e3fa370931a5ac8e8085ec81a09795403f09e3fe
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2013-12-04 21:23:16 +0100

    Add all other roles and better role management

--- app/models/person.rb
+++ app/models/person.rb
@@ -12,6 +12,10 @@ class Person < ActiveRecord::Base
       .references(movie: :movie_years)
   end
 
+  def as_noncast_base
+    occupations.where("role_id NOT IN (?)", Role.cast_roles)
+  end
+
   def as_self_base
     as_cast.where("character_norm ~ E'(himself|herself|themselves)'")
       .where("occupations.id NOT IN (#{as_archive_base.select("occupations.id").to_sql})")
@@ -42,15 +46,29 @@ class Person < ActiveRecord::Base
     cast_order(as_self_base)
   end
 
-  def as_role(role)
-    case role
+  def as_noncast(role_name)
+    occupations.where("role_id IN (#{Role.where(role: role_name).select(:id).to_sql})").includes(:movie).joins(movie: :movie_years)
+      .references(movie: :movie_years)
+  end
+
+  def as_role(role_name)
+    case role_name
     when "acting"
       return as_acting
     when "self"
       return as_self
     when "archive"
       return as_archive
+    else
+      cast_order(as_noncast(role_name))
     end
+  end
+
+  def active_role_names
+    as_noncast_base.select("DISTINCT(roles.*)")
+      .joins(:role)
+      .sort_by { |x| Role::GROUP_SORT_ORDER[x.group] }
+      .map { |x| x.attributes["role"] }
   end
 
   def cast_hashes
@@ -59,6 +77,19 @@ class Person < ActiveRecord::Base
       as_self: as_hash(as_self),
       as_archive: as_hash(as_archive)
     }
+  end
+
+  def all_roles
+    roles = ["acting", "self", "archive"]
+    roles + Role.all_noncast_role_names
+  end
+
+  def active_roles
+    roles = []
+    roles << "acting" if as_acting_base.count > 0
+    roles << "self" if as_self_base.count > 0
+    roles << "archive" if as_archive_base.count > 0
+    roles + active_role_names
   end
 
   def as_hash(query)
@@ -76,6 +107,8 @@ class Person < ActiveRecord::Base
   def as_json(options)
     super(options)
       .merge({
+               all_roles: all_roles,
+               active_roles: active_roles,
                score: @score
              }).compact
   end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,15 @@
 class Role < ActiveRecord::Base
+  GROUP_SORT_ORDER={ 1 => 1, 2 => 3, 3 => 2, 4 => 4 }
   has_many :occupations
 
   def self.cast_roles
     @@cast_roles ||= Role.where(role: ['actor', 'actress']).map(&:id)
+  end
+
+  def self.all_noncast_role_names
+    @@all_noncast_role_names ||= Role.order(:group, :id)
+      .where("\"group\" >= 2")
+      .sort_by { |x| GROUP_SORT_ORDER[x.group] }
+      .map(&:role)
   end
 end
